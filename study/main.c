471e4fdf1fbaee5e517e8538c05495df9aa156e1
selnem <tjdghks0130@naver.com>
2025-11-21 11:09:07 +0900

    h

--- study/main.c
+++ study/main.c
@@ -135,7 +135,7 @@ int main(void) {
             // Null-terminate the string
             rx_buffer[n_read] = '\0';
             
-            prinnt("%s"rx_buffer);
+            prinnt("%s",rx_buffer);
             if(rx_buffer[n_read-1]=='\n'){
                 break;
             }
