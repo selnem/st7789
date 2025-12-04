484476c342957fca577635595efdf1315d4749e4
selnem <tjdghks0130@naver.com>
2025-12-04 23:24:20 +0900

    feat:Map,pipe slide logic

---
+++ game.h
@@ -0,0 +1,11 @@
+#pragma once
+
+void map_slide();
+void pipes_slide();
+void reset_map_slide();
+void reset_pipes_slide();
+int get_map_slide_idx();
+int get_pipes_slide_idx();
+void set_slide_speed(int speed);
+void update_hitbox_screen(int airplane_x, int airplane_y);
+
---
+++ hitbox.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void reset_hitbox_screen();
+void set_hitbox_pipes(const bitmap* obj, int idx, int y_offset);
+int set_hitbox_airplane(int x, int y, const bitmap* obj);  // 반환값: 충돌 여부 (1=충돌, 0=없음)
+int check_collision();
+int check_collision_at_position(int x, int y, const bitmap* obj);
+
